8c750cccb0f0cf268aac24c01af836d0c22edd35
Badjo <bart@badjo.nl>
2024-03-21 12:45:33 +0100

    add javascript library local-date.js

---
+++ app/javascript/application.js
@@ -0,0 +1,3 @@
+import LocalTime from "local-time"
+console.log('init playground app')
+LocalTime.start()
---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "local-time" # @3.0.2
